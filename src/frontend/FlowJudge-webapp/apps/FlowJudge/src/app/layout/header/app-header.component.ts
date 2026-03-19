d102c19306c8fc376f40d2d9c170e6964c68503a
wbiegala <wojciech.biegala@gmail.com>
2026-03-19 20:07:05 +0100

    Better styling

--- src/frontend/FlowJudge-webapp/apps/FlowJudge/src/app/layout/header/app-header.component.ts
+++ src/frontend/FlowJudge-webapp/apps/FlowJudge/src/app/layout/header/app-header.component.ts
@@ -4,10 +4,11 @@ import { HeaderProgressBarComponent } from './header-progress-bar/header-progres
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { HeaderUserPanelComponent } from './header-user-panel/header-user-panel.component';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  imports: [HeaderProgressBarComponent, HeaderUserPanelComponent, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [HeaderProgressBarComponent, HeaderUserPanelComponent, MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './app-header.component.html',
   styleUrl: './app-header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
